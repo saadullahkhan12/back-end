b56840ce30e76379e52252406c81314736503a38
saadullahkhan12 <muhammadsaadullah093@gmail.com>
2024-12-20 10:31:41 +0500

    saadsssss

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const task = [
     { id: 1, name: "Task 1", completed: false },
     { id: 2, name: "Task 2", completed: false },
     { id: 3, name: "Task 3", completed: true },
-    { id: 4, name: "Task 4", completed: false },
+    { id: 4, name: "Task 5", completed: false },
     
 ]
 
